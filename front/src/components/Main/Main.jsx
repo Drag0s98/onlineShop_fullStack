a6b20b2993eb266a2a9fb309a62e0a9763303d66
Drag0s98 <dragostanase1698@gmail.com>
2021-10-12 18:05:47 +0200

    card view created

--- front/src/components/Main/Main.jsx
+++ front/src/components/Main/Main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Route, Switch } from 'react-router-dom';
 
 import Home from '../Home';
-import Error from '../Error';
 import Auth from '../Auth';
-
+import Cart from '../Cart'
+import Error from '../Error';
 
 const Main = () => {
   return(
@@ -12,6 +12,7 @@ const Main = () => {
       <Switch>
         <Route path='/' component={Home} exact/>
         <Route path='/auth' component={Auth} />
+        <Route path='/cart' component={Cart} />
         <Route component={Error} />
       </Switch>
     </main>
